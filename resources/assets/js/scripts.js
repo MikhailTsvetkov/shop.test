118cfb628e72c6733e960c81b7c4527c34c8a011
MikhailTsvetkov <vandermust@gmail.com>
2024-06-23 21:55:05 +0300

    Fix scripts.js

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -1,4 +1,4 @@
-document.ready(function () {
+$(document).ready(function () {
     // Показ формы отправки отзыва
     $(document).on('click', '#testimonial-open', function () {
         $('#testimonial-form').slideDown();
